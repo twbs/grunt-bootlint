85f669d2b328a2f53d2a9ea7eb2a5b7774a63e70
Zac Echola <zac.echola@so.mnscu.edu>
2014-11-14 14:55:43 -0600

    remove bootlint tasks from grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -110,7 +110,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'bootlint', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
